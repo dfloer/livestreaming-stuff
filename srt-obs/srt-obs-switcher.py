0800cbaf1e2bf4a1f002330356425f8376be0e9f
Dale Floer <dalefloer@gmail.com>
2020-11-17 19:15:44 -0800

    First pass at network drop and BRB switching.
    It works well enough.

---
+++ srt-obs/srt-obs-switcher.py
@@ -0,0 +1,109 @@
+from srt_stats import SRTThread
+import toml
+from obswebsocket import obsws, requests
+from dataclasses import dataclass
+from time import sleep
+from itertools import chain
+
+
+def get_config(config_file="srt_config.toml"):
+    with open(config_file, 'r') as f:
+        return toml.load(f)
+
+class OBSControl(object):
+    def __init__(self, obs_cfg):
+        self.config = obs_cfg
+        self.ws = None
+        self.normal_scene = self.config["scene_name"]
+        self.brb_scene = self.config["brb_scene_name"]
+        self.connect()
+        self.scenes = None
+        self.get_scenes()
+
+    def connect(self):
+        self.ws = obsws(self.config["websocket_host"], self.config["websocket_port"], self.config["websocket_secret"])
+        self.ws.connect()
+
+    def disconnect(self):
+        self.ws.disconnect()
+
+    def get_scenes(self):
+        self.scenes = self.ws.call(requests.GetSceneList())
+
+    def go_brb(self):
+        print("brb")
+        self.ws.call(requests.SetCurrentScene(self.brb_scene))
+
+    def go_normal(self):
+        print("normal")
+        self.ws.call(requests.SetCurrentScene(self.normal_scene))
+
+    @property
+    def current_scene(self):
+        return self.ws.call(requests.GetCurrentScene()).getName()
+
+    def __str__(self):
+        return f"Scenes: {self.scenes}, normal name: {self.normal_scene}, brb name: {self.brb_scene}."
+
+
+if __name__ == "__main__":
+    config = get_config()
+
+    srt_cfg = config["srt_relay"]
+    srt_thread = SRTThread(f"srt://:{srt_cfg['output_port']}", f"srt://:{srt_cfg['listen_port']}")
+
+    srt_thread.daemon = True
+    srt_thread.start()
+
+    obs_cfg = config["obs"]
+    thresholds = config["brb_thresholds"]
+
+    obs_ctrl = OBSControl(obs_cfg)
+
+    brb_scene = obs_cfg["brb_scene_name"]
+
+    stabilize_dec = thresholds["check_interval"]
+    stabilize_countdown = 0
+
+    # Make sure we're on our live scene.
+    obs_ctrl.go_normal()
+
+    ra_samples = thresholds["running_avg"]
+    rtt_samples = [None for _ in range(ra_samples)]
+    bitrate_samples = [None for _ in range(ra_samples)]
+    try:
+        idx = 0
+        while True:
+            current_scene = obs_ctrl.current_scene
+            print(f"Current scene: {current_scene}, countdown: {stabilize_countdown}.")
+            stats = srt_thread.last_stats
+            if stats == {}:
+                continue
+
+            rtt_samples[idx % ra_samples] = stats["link"]["rtt"]
+            bitrate_samples[idx % ra_samples] = stats["send"]["mbitRate"]
+            rtt_ra = sum([x for x in rtt_samples if x])
+            bitrate_ra = sum([x for x in bitrate_samples if x])
+
+            healthy = rtt_ra <= thresholds["rtt"] and bitrate_ra >= thresholds["bitrate"]
+            if "SRT source disconnected" in srt_thread.last_message:
+                healthy = False
+            print(f"rtt: {rtt_ra}, bitrate: {bitrate_ra}, healthy: {healthy}.")
+
+            if healthy:
+                if stabilize_countdown >= 0.0:
+                    stabilize_countdown -= stabilize_dec
+                elif current_scene == brb_scene and stabilize_countdown <= 0.0:
+                    obs_ctrl.go_normal()
+            elif current_scene != brb_scene:
+                obs_ctrl.go_brb()
+                stabilize_countdown = thresholds["stabilize_time"]
+            else:
+                stabilize_countdown = thresholds["stabilize_time"]
+
+            sleep(thresholds["check_interval"])
+    except KeyboardInterrupt:
+        pass
+
+    obs_ctrl.disconnect()
+    srt_thread.stop()
---
+++ srt-obs/srt_stats.py
@@ -0,0 +1,97 @@
+import threading
+import subprocess
+import select
+import json
+from os import set_blocking
+
+
+class SRTThread(threading.Thread):
+    def __init__(self, srt_destination, srt_source="udp://:4200", stats_interval=100, update_interval=0.1):
+        """
+        Wrapper thread to start/stop srt-live-transmit and get stats out of it.
+        Source and destination as per documentation at: https://github.com/Haivision/srt/blob/master/docs/srt-live-transmit.md
+        Args:
+            srt_destination (str): Destination srt server to send to.
+            srt_source (str, optional): Port and protocol that srt-live-transmit listens on. Defaults to "udp://:4200"
+                Ideally this would be using SRT, but the build of gstreamer that comes with the Jetson doesn't support it.
+            stats_interval (int, optional): How often to update the SRT stats, in _packets_, not time. Defaults to 100.
+            update_interval (float, optional): How often to should read stats from the process, too often and it blocks the web thread, not often enough and output from the process gets blocked.. Defaults to 0.1.
+        """
+        self.event = threading.Event()
+        self.stats_interval = stats_interval
+        self.update_interval = update_interval
+        self.src_conn = srt_source
+        self.dst_conn = srt_destination
+        self.srt_process = self.start_process()
+        set_blocking(self.srt_process.stdout.fileno(), False)
+        self.last_message = ''
+        self.last_stats = {}
+        super().__init__(group=None)
+
+    def run(self):
+        """
+        Get the stats and save the last one to this object.
+        """
+        while not self.event.is_set():
+            stats, msg = self.stats_parse()
+            if stats:
+                self.last_stats = stats[-1]
+            if msg:
+                self.last_message = msg[-1]
+                print(f"Message: {msg}")
+            self.event.wait(self.update_interval)
+
+    def start_process(self):
+        """
+        Start the SRT process.
+        """
+        srt_cmd = f"srt-live-transmit -srctime -buffering 1 -s {self.stats_interval} -pf json {self.src_conn} {self.dst_conn}"
+        return subprocess.Popen(
+            f"{srt_cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+
+    def kill_process(self):
+        """
+        Start the SRT process.
+        """
+        self.srt_process.kill()
+
+    def get_raw_stats(self):
+        """
+        Get the raw stats from the SRT process.
+        Returns:
+            bytestring: Raw output strings from srt-live-transmit's stderr and stdout.
+        """
+        res = []
+        idx = 0
+        while True:
+            idx += 1
+            l = self.srt_process.stdout.readline()
+            if l == b'':
+                break
+            res += [l]
+        return res
+
+
+    def stats_parse(self):
+        """
+        Parses a raw message from srt-live-transmit, into either a dict or a message.
+        Returns:
+            tuple: message (empty if there isn't one, making this json only), dictionary (empty, otherwise decoded json).
+        """
+        raw_stats = self.get_raw_stats()
+        messages = []
+        stats = []
+        for line in raw_stats:
+            try:
+                stats += [json.loads(line)]
+            except json.decoder.JSONDecodeError:
+                messages += [line.decode('ASCII')]
+        return stats, messages
+
+    def stop(self):
+        """
+        Stops the srt-live-transmit process and the stats-gathering loop.
+        """
+        self.kill_process()
+        self.event.set()
