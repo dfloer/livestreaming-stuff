e2927ddc07eb08c4f4af9a949ad7d52232921a5f
Dale Floer <dalefloer@gmail.com>
2022-02-24 02:14:36 -0800

    Refactor of SRT stats gathering thread.
    This was mostly to clean things up so that the underlying process could
    be cleanly restarted to work around a potential SRT bug related to it
    complaining about the time on the sender and receiver being out of sync
    somehow.
    The error is "Wrong source time was provided.Sending is rejected.", and
    only appearred after running for hours (potentially idle, I haven't done
    a test leaving it sending for hours to see if the error case happens
    then too).

--- srt-obs/srt_stats.py
+++ srt-obs/srt_stats.py
@@ -1,22 +1,19 @@
 import threading
 import subprocess
-import select
 import json
-from os import set_blocking
-import secrets
-from datetime import datetime
+from os import set_blocking, getpgid
+from datetime import datetime, timedelta
 from loguru import logger as logging
+from utils import ThreadManager, get_passphrase
 
-
-class SRTThread(threading.Thread):
-    def __init__(self, srt_destination, srt_source="udp://:4200", stats_interval=100, update_interval=0.1, passphrase='', srt_live_transmit="srt-live-transmit", loss_max_ttl=50, srt_latency=2000):
+class SRTThread(ThreadManager):
+    def __init__(self, srt_destination, srt_source, stats_interval=100, update_interval=0.1, passphrase='', srt_live_transmit="srt-live-transmit", loss_max_ttl=50, srt_latency=2000):
         """
         Wrapper thread to start/stop srt-live-transmit and get stats out of it.
         Source and destination as per documentation at: https://github.com/Haivision/srt/blob/master/docs/srt-live-transmit.md
         Args:
             srt_destination (str): Destination srt server to send to.
-            srt_source (str, optional): Port and protocol that srt-live-transmit listens on. Defaults to "udp://:4200"
-                Ideally this would be using SRT, but the build of gstreamer that comes with the Jetson doesn't support it.
+            srt_source (str): Port and protocol that srt-live-transmit listens on.
             stats_interval (int, optional): How often to update the SRT stats, in _packets_, not time. Defaults to 100.
             update_interval (float, optional): How often to should read stats from the process, too often and it blocks the web thread, not often enough and output from the process gets blocked.. Defaults to 0.1.
             passphrase (str, optional): Passphrase to use for encryption. If this is blank, one will be generated and printed on the console.
@@ -24,90 +21,71 @@ class SRTThread(threading.Thread):
             loss_max_ttl (int, optional): Tolerance to packet re-ordering. Defaults to 50.
             srt_latency (int, optional): Maximum acceptable transmission latency. If we go past this, drop the packets. Defaults to 200.
         """
-        self.event = threading.Event()
+        super().__init__()
         self.stats_interval = stats_interval
         self.update_interval = update_interval
+        self.passphrase = get_passphrase(passphrase)
+        self.last_message = ''
+        self.last_stats = {}
+        self._name = "SLT"
+        self.last_update = datetime.now()
+        self.connected = False
+        # srt-live-transmit command variables
         self.srt_exec = srt_live_transmit
         self.loss_max_ttl = loss_max_ttl
         self.srt_latency = srt_latency
-        self.last_update = datetime.now()
-        self.connected = False
-        self.start_time = None
-
-        self.passphrase = passphrase
-        if not self.passphrase:
-            logging.debug("Generated SRT passphrase.")
-            self.generate_passphrase()
-        div = "----------------------------------------------------------------"
-        print(f"\nSRT Passphrase:")
-        print(div)
-        print(f"{self.passphrase}")
-        print(div)
-
         self.src_conn = f"{srt_source}?passphrase={self.passphrase}&enforcedencryption=true&mode=listener&lossmaxttl={self.loss_max_ttl}&latency={self.srt_latency}"
         self.dst_conn = srt_destination
-        self.srt_process = self.start_process()
-        set_blocking(self.srt_process.stdout.fileno(), False)
-        self.last_message = ''
-        self.last_stats = {}
-        super().__init__(group=None)
+        # Generate the actual command
+        self.cmd = f"{self.srt_exec} -srctime -buffering 1 -s {self.stats_interval} -pf json \"{self.src_conn}\" {self.dst_conn}"
+        self.start_process()
 
-    def generate_passphrase(self, length=32):
-        """
-        Generates a passphrase for use with SRT.
-        Args:
-            length (int, optional): Length of the passphrase to generate. Default 32.
-        Returns:
-            (str) the passphrase.
-        """
-        self.passphrase = secrets.token_hex(length)
-
-    def run(self):
+    def run_inner(self):
         """
         Get the stats and save the last one to this object.
         """
-        logging.info("SRT thread started.")
-        while not self.event.is_set():
-            stats, msg = self.stats_parse()
-            if stats:
-                self.last_stats = stats[-1]
-                self.last_update = datetime.now()
-                logging.debug(f"SRT raw stats: {stats}")
-                # If the stats have updated, assume that SRT is connected.
-                self.connected = True
-            if msg:
-                self.last_message = msg[-1]
-                logging.info(f"SRT Message: {msg}")
+        stats, msg = self.stats_parse()
+        if stats:
+            self.last_stats = stats[-1]
+            self.last_update = datetime.now()
+            logging.debug(f"SRT raw stats: {stats}")
+            # If the stats have updated, assume that SRT is connected.
+            self.connected = True
+        if msg:
+            self.last_message = msg[-1]
+            logging.info(f"SRT Message: {msg}")
 
-            # Track connection state, because the switcher uses it to determine health.
-            # This is only for the incoming connection, not the target connection.
-            # If that were to break, well, OBS is broken and there isn't much to do...
-            if "SRT source disconnected" in self.last_message and self.connected:
-                self.connected = False
-                logging.warning(f"SRT: Source Disconnected.")
-            elif "Accepted SRT source connection" in self.last_message and not self.connected:
-                self.connected = True
-                logging.warning(f"SRT: Source Connected.")
+        # SRT connection and process health checks.
+        self.check_is_connected()
+        self.check_time_workaround()
 
-            self.event.wait(self.update_interval)
+    def run(self):
+        self.wait_interval = self.update_interval
+        super().run()
 
-    def start_process(self):
+    def check_is_connected(self):
         """
-        Start the SRT process.
+        Track connection state, because the switcher uses it to determine health.
+        This is only for the incoming connection, not the target connection.
+        If that were to break, well, OBS is broken and there isn't much to do...
         """
-        srt_cmd = f"{self.srt_exec} -srctime -buffering 1 -s {self.stats_interval} -pf json \"{self.src_conn}\" {self.dst_conn}"
-        logging.info(f"Starting SRT with command: {srt_cmd}")
-        self.start_time = datetime.now()
-        return subprocess.Popen(
-            f"{srt_cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        )
+        if "SRT source disconnected" in self.last_message and self.connected:
+            self.connected = False
+            logging.warning(f"SRT: Source Disconnected.")
+        elif "Accepted SRT source connection" in self.last_message and not self.connected:
+            self.connected = True
+            logging.warning(f"SRT: Source Connected.")
 
-    def kill_process(self):
+    def check_time_workaround(self):
         """
-        Kill the SRT process.
+        This is a workaround for a clock drift (or something) but in srt-live-transmit.
+        It usually takes several (8+) hours to show up in testing.
+        Restarting the srt-live-transmit process should fix this.
         """
-        logging.warning(f"Killing SRT process running since: {self.start_time}")
-        self.srt_process.kill()
+        if "Wrong source time was provided" in self.last_message:
+            logging.error(f"SRT Live Transmit source time error. Process started: {self.start_time}.")
+            self.restart_process()
+            logging.warning("SRT Live Transmit recovered from source time error.")
 
     def get_raw_stats(self):
         """
@@ -119,12 +97,11 @@ class SRTThread(threading.Thread):
         idx = 0
         while True:
             idx += 1
-            l = self.srt_process.stdout.readline()
+            l = self.readline()
             if l == b'':
                 break
             res += [l]
         return res
-
 
     def stats_parse(self):
         """
@@ -141,14 +118,6 @@ class SRTThread(threading.Thread):
             except json.decoder.JSONDecodeError:
                 messages += [line.decode('ASCII')]
         return stats, messages
-
-    def stop(self):
-        """
-        Stops the srt-live-transmit process and the stats-gathering loop.
-        """
-        self.kill_process()
-        self.event.set()
-        logging.info("Stopping stats listener process.")
 
 class SRTLAThread(threading.Thread):
     def __init__(self, srtla_rec="srtla_rec", source_port=4000, destination_host="localhost", destination_port=4001):
